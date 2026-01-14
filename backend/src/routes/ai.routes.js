c0b594cc5d1ea932e1a441365dc8574e8ad5c464
Anurag <yadavanu9580@gmail.com>
2026-01-14 18:41:10 +0530

    folder move

--- backend/src/routes/ai.routes.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const aiControllers = require('../controllers/ai.controllers');
-const router = express.Router();
-
-router.get("/get-response", aiControllers.getResponse)
-module.exports = router;
